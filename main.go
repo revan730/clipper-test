adf526175bf117abac346fd4bdf89b2248b570f1
Evgeniy Burlachenko <revan730@gmail.com>
2019-01-25 21:57:44 +0200

    One more test of CD

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
-  fmt.Printf("Starting server at port 8080")
+  fmt.Printf("Server running at port 8080")
 	http.ListenAndServe(":8080", nil)
 }
